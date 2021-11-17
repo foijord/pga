d1ed9b0229d6b52a0d97cb6cf086d1f3348591e2
Frode Oijord <foijord@gmail.com>
2021-11-17 08:07:04 +0100

    rewrite

--- pga.cpp
+++ pga.cpp
@@ -1,6 +1,7 @@
 
 #include <vector>
 #include <ranges>
+#include <numbers>
 #include <iostream>
 #include <algorithm>
 
@@ -41,27 +42,12 @@ namespace pga
 
 
 	struct line {
-		struct {
-			union { float x, e41; };
-			union { float y, e42; };
-			union { float z, e43; };
-		} v;
-
-		struct {
-			union { float x, e23; };
-			union { float y, e31; };
-			union { float z, e12; };
-		} m;
-
-		point weight() const
-		{
-			return point{
-				.e1 = v.e41,
-				.e2 = v.e42,
-				.e3 = v.e43,
-				.e4 = 0,
-			};
-		}
+		union { float mx, e23; };
+		union { float my, e31; };
+		union { float mz, e12; };
+		union { float vx, e41; };
+		union { float vy, e42; };
+		union { float vz, e43; };
 	};
 
 
@@ -70,147 +56,252 @@ namespace pga
 		union { float y, e314; };
 		union { float z, e124; };
 		union { float w, e321; };
-
-		point weight() const
-		{
-			return point{
-				.e1 = e234,
-				.e2 = e314,
-				.e3 = e124,
-				.e4 = 1,
-			};
-		}
 	};
 
 
-	struct motor {
-		motor(const line& l, float phi, float d)
-		{
-			const auto& v = l.v;
-			const auto& m = l.m;
+	template <typename T>
+	inline bool operator==(const T& a, const T& b)
+	{
+		return a.x == b.x && a.y == b.y && a.z == b.z && a.w == b.w;
+	}
 
-			this->r = {
-				.e41 = v.x * sin(phi),
-				.e42 = v.y * sin(phi),
-				.e43 = v.z * sin(phi),
-				.e1234 = cos(phi),
-			};
 
-			this->u = {
-				.e23 = d * v.x * cos(phi) + m.x * sin(phi),
-				.e31 = d * v.y * cos(phi) + m.y * sin(phi),
-				.e12 = d * v.z * cos(phi) + m.z * sin(phi),
-				.scalar = -d * sin(phi),
-			};
-		}
+	inline bool operator==(const line& a, const line& b)
+	{
+		return
+			a.mx == b.mx && a.my == b.my && a.mz == b.mz &&
+			a.vx == b.vx && a.vy == b.vy && a.vz == b.vz;
+	}
 
-		struct {
-			union { float x, e41; };
-			union { float y, e42; };
-			union { float z, e43; };
-			union { float w, e1234, antiscalar; };
-		} r;
 
-		struct {
-			union { float x, e23; };
-			union { float y, e31; };
-			union { float z, e12; };
-			union { float w, scalar; };
-		} u;
-	};
+	// ===============================================================================
+	// Join and Meet
+	// ===============================================================================
 
 
 	// line containing points p and q.
-	// zero if p and q are coincident.
-	line operator^(const point& p, const point& q)
+	// zero if p and q are coincident
+	inline line operator^(const point& p, const point& q)
 	{
-		return line{
-			.v = {
-				.e41 = (q.x * p.w - p.x * q.w),
-				.e42 = (q.y * p.w - p.y * q.w),
-				.e43 = (q.z * p.w - p.z * q.w),
-			},
-			.m = {
-				.e23 = (p.y * q.z - p.z * q.y),
-				.e31 = (p.z * q.x - p.x * q.z),
-				.e12 = (p.x * q.y - p.y * q.x),
-			},
+		return {
+			.e23 = p.y * q.z - p.z * q.y,
+			.e31 = p.z * q.x - p.x * q.z,
+			.e12 = p.x * q.y - p.y * q.x,
+			.e41 = p.w * q.x - p.x * q.w,
+			.e42 = p.w * q.y - p.y * q.w,
+			.e43 = p.w * q.z - p.z * q.w,
 		};
 	}
 
 
 	// plane containing line l and point p.
-	// normal is zero if p lies in l
-	plane operator^(const line& l, const point& p)
+	// normal is zero if p lies in l.
+	inline plane operator^(const line& l, const point& p)
 	{
-		const auto& v = l.v;
-		const auto& m = l.m;
-
 		return {
-			.e234 = +(v.y * p.z - v.z * p.y + m.x * p.w),
-			.e314 = +(v.z * p.x - v.x * p.z + m.y * p.w),
-			.e124 = +(v.x * p.y - v.y * p.x + m.z * p.w),
-			.e321 = -(m.x * p.x + m.y * p.y + m.z * p.z),
+			.e234 = l.vy * p.z - l.vz * p.y + l.mx * p.w,
+			.e314 = l.vz * p.x - l.vx * p.z + l.my * p.w,
+			.e124 = l.vx * p.y - l.vy * p.x + l.mz * p.w,
+			.e321 = -(l.mx * p.x + l.my * p.y + l.mz * p.z),
 		};
 	}
 
 
 	// line where planes f and g intersect.
 	// direction is zero if f and g are parallel.
-	line operator&(const plane& f, const plane& g)
+	inline line operator&(const plane& f, const plane& g)
 	{
-		return line{
-			.v = {
-				.e41 = (f.z * g.y - f.y * g.z),
-				.e42 = (f.x * g.z - f.z * g.x),
-				.e43 = (f.y * g.x - f.x * g.y),
-			},
-			.m = {
-				.e23 = (f.x * g.w - g.x * f.w),
-				.e31 = (f.y * g.w - g.y * f.w),
-				.e12 = (f.z * g.w - g.z * f.w),
-			},
+		return {
+			.e23 = f.x * g.w - f.w * g.x,
+			.e31 = f.y * g.w - f.w * g.y,
+			.e12 = f.z * g.w - f.w * g.z,
+			.e41 = f.z * g.y - f.y * g.z,
+			.e42 = f.x * g.z - f.z * g.x,
+			.e43 = f.y * g.x - f.x * g.y,
 		};
 	}
 
 
 	// point where line l intersects plane f.
 	// weight is zero if l and f are parallel.
-	point operator&(const line& l, const plane& f)
+	inline point operator&(const line& l, const plane& f)
 	{
-		const auto& v = l.v;
-		const auto& m = l.m;
+		return {
+			.e1 = l.my * f.z - l.mz * f.y + l.vx * f.w,
+			.e2 = l.mz * f.x - l.mx * f.z + l.vy * f.w,
+			.e3 = l.mx * f.y - l.my * f.x + l.vz * f.w,
+			.e4 = -(l.vx * f.x + l.vy * f.y + l.vz * f.z),
+		};
+	}
 
-		return point{
-			.e1 = +(m.y * f.z - m.z * f.y + v.x * f.w),
-			.e2 = +(m.z * f.x - m.x * f.z + v.y * f.w),
-			.e3 = +(m.x * f.y - m.y * f.x + v.z * f.w),
-			.e4 = -(v.x * f.x + v.y * f.y + v.z * f.z),
+
+	inline point operator&(const plane& f, const line& l)
+	{
+		return l & f;
+	}
+
+	// ===============================================================================
+	// Dualization
+	// ===============================================================================
+
+	// plane at infinity
+	inline plane operator!(const point& p)
+	{
+		return {
+			.e234 = 0,
+			.e314 = 0,
+			.e124 = 0,
+			.e321 = -p.w,
+		};
+	}
+
+	// line at infinity perpendicular to line l.
+	inline line operator!(const line& l)
+	{
+		return {
+			.e23 = -l.vx,
+			.e31 = -l.vy,
+			.e12 = -l.vz,
+			.e41 = 0,
+			.e42 = 0,
+			.e43 = 0,
+		};
+	}
+
+	// point at infinity perpendicular to plane f.
+	inline point operator!(const plane& f)
+	{
+		return {
+			.e1 = f.x,
+			.e2 = f.y,
+			.e3 = f.z,
+			.e4 = 0,
 		};
 	}
 }
 
 
-bool line_from_points(pga::point a, pga::point b, pga::line line)
+bool test_line_from_plane_and_point()
 {
 	return [&] {
-		auto result = a ^ b;
-		return true;
+		pga::point a{ 1, 0, 0, 1 };
+		pga::point b{ 0, 1, 0, 1 };
+		pga::point c{ 0, 0, 1, 1 };
+
+		pga::plane f = a ^ b ^ c;
+		pga::point p{ 1, 1, 1, 1 };
+
+		// line perpendicular to plane f and passing through point p.
+		// direct formula, equivalent to !f ^ p
+		pga::line l = {
+			.e23 = f.y * p.z - f.z * p.y,
+			.e31 = f.z * p.x - f.x * p.z,
+			.e12 = f.x * p.y - f.z * p.x,
+			.e41 = -f.x * p.w,
+			.e42 = -f.y * p.w,
+			.e43 = -f.z * p.w,
+		};
+		// compare result of direct formula with result obtained using dualization
+		return l == (!f ^ p);
 	}();
 }
 
 
-bool plane_from_points()
+bool test_plane_from_line_and_point()
 {
 	return [&] {
-		pga::point a{ 0, 0, 0, 1 };
+		pga::point a{ 1, 0, 0, 1 };
+		pga::point b{ 0, 1, 1, 1 };
+
+		pga::line l = a ^ b;
+		pga::point p{ 1, 1, 1, 1 };
+
+		// plane perpendicular to line l and containing point p.
+		// direct formula, equivalent to !l ^ p
+		pga::plane f = {
+			.e234 = -l.vx * p.w,
+			.e314 = -l.vy * p.w,
+			.e124 = -l.vz * p.w,
+			.e321 = l.vx * p.x + l.vy * p.y + l.vz * p.z,
+		};
+		// compare result of direct formula with result obtained using dualization
+		return f == (!l ^ p);
+	}();
+}
+
+
+bool test_plane_from_plane_and_line()
+{
+	return [&] {
+		// xy plane
+		pga::point a{ 1, 0, 0, 1 };
 		pga::point b{ 0, 1, 0, 1 };
-		pga::point c{ 1, 1, 0, 1 };
+		pga::point c{ 0, 0, 0, 1 };
+		pga::plane f = a ^ b ^ c;
 
-		auto plane = a ^ b ^ c;
-		auto normal = plane.weight();
+		// line in xz plane
+		pga::point p{ 1, 0, 0, 1 };
+		pga::point q{ 0, 0, 1, 1 };
+		pga::line l = p ^ q;
 
-		return true;
+		// plane perpendicular to plane f and containing line l.
+		// direct formula, equivalent to l ^ !f
+		pga::plane g = {
+			.e234 = l.vy * f.z - l.vz * f.y,
+			.e314 = l.vz * f.x - l.vx * f.z,
+			.e124 = l.vx * f.y - l.vy * f.x,
+			.e321 = -(l.mx * f.x + l.my * f.y + l.mz * f.z),
+		};
+		// compare result of direct formula with result obtained using dualization
+		return g == (l ^ !f);
+	}();
+}
+
+
+bool test_project_point_onto_plane()
+{
+	return [&] {
+		// xy plane
+		pga::point a{ 1, 0, 0, 1 };
+		pga::point b{ 0, 1, 0, 1 };
+		pga::point c{ 0, 0, 0, 1 };
+		pga::plane f = a ^ b ^ c;
+
+		// point above xy plane
+		const pga::point p{ 1, -1, 1, 1 };
+
+		float f2 = f.x * f.x + f.y * f.y + f.z * f.z;
+		float fp = f.x * p.x + f.y * p.y + f.z * p.z + f.w * p.w;
+		pga::point pf = {
+			 .e1 = f2 * p.x - fp * f.x,
+			 .e2 = f2 * p.y - fp * f.y,
+			 .e3 = f2 * p.z - fp * f.z,
+			 .e4 = p.w,
+		};
+		// compare result of direct formula with result obtained using dualization
+		return pf == (((!f) ^ p) & f);
+	}();
+}
+
+
+bool test_project_point_onto_line()
+{
+	return [&] {
+		pga::point a{ 1, 0, 0, 1 };
+		pga::point b{ 0, 1, 0, 1 };
+		pga::line l = a ^ b;
+
+		// point above xy plane
+		const pga::point p{ 1, 1, 1, 1 };
+
+		pga::point pl = {
+			 .e1 = (l.vy * l.mz - l.vz * l.my) * p.w,
+			 .e2 = (l.vz * l.mx - l.vx * l.mz) * p.w,
+			 .e3 = (l.vx * l.my - l.vy * l.mx) * p.w,
+			 .e4 = l.vx * p.x + l.vy * p.y + l.vz * p.z + (l.vx * l.vx + l.vy * l.vy + l.vz * l.vz) * p.w,
+		};
+		// compare result of direct formula with result obtained using dualization
+		return pl == (((!l) ^ p) & l);
 	}();
 }
 
@@ -218,8 +309,11 @@ bool plane_from_points()
 int main(int argc, char** argv)
 {
 	std::vector<bool> tests{
-		line_from_points({ 2, 3, 7, 1 }, { 2, 1, 0, 1 }, { 1, 0, 0, 0, 0, 0 }),
-		plane_from_points(),
+		test_line_from_plane_and_point(),
+		test_plane_from_line_and_point(),
+		test_plane_from_plane_and_line(),
+		test_project_point_onto_plane(),
+		test_project_point_onto_line(),
 	};
 
 	std::cout << tests.size() << " tests executed." << std::endl;
